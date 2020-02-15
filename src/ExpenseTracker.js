c8682859def4bbd7d6550bfb659dd8898ba9a114
ram071985 <reidmuchow@gmail.com>
2020-02-15 17:59:01 -0600

    added changes to ExpenseTracker.js

--- src/ExpenseTracker.js
+++ src/ExpenseTracker.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Col, Form, Row, Button, ButtonToolbar, Table } from "react-bootstrap";
+import {
+  Container,
+  Col,
+  Form,
+  Row,
+  Button,
+  ButtonToolbar,
+  Table
+} from "react-bootstrap";
 
 class ExpenseTracker extends Component {
   constructor() {
@@ -9,35 +17,51 @@ class ExpenseTracker extends Component {
       type: "",
       name: "",
       date: "",
-      amount: ""
+      amount: "",
+      expenses: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
+  
   handleChange(event) {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   }
 
-  TableRow() {
-    return (
-      //if ()
-      <Table striped bordered hover variant="dark">
-        <tbody>
-          <tr>
-            <td>{this.state.type}</td>
-            <td>{this.state.name}</td>
-            <td>{this.state.date}</td>
-            <td>{this.state.amount}</td>
-          </tr>
-        </tbody>
-      </Table>
-    );
-  }
+ /* TableRow() {
+    const rows = this.state.expenses.map(function(expense) {
+      return (
+        <tr>
+          <td>{this.state.type}</td>
+          <td>{this.state.name}</td>
+          <td>{this.state.date}</td>
+          <td>{this.state.amount}</td>
+        </tr>
+      );
+    });
+    // rows should equal an array
+    // of jsx <row>'s
+    
+  }*/
 
   handleClick() {
-     
+    const addRow = {
+      type: this.state.type,
+      name: this.state.name,
+      date: this.state.date,
+      amount: this.state.amount
+    }
+    console.log(addRow)
+    this.setState({expenses: addRow})
+    console.log()
+
+    // add to the empty array in Expenses: []
+    // using an object constructor
+    // this.setState(this.state.expenses)
+
   }
+
 
   render() {
     return (
@@ -99,7 +123,7 @@ class ExpenseTracker extends Component {
         <Row className="justify-content-md-center mt-5">
           <Col md="auto">
             <ButtonToolbar>
-              <Button onClick={this.handleClick} variant="success" size="lg">
+              <Button variant="success" size="lg" onClick={this.handleClick}>
                 Add Expense
               </Button>
             </ButtonToolbar>
@@ -114,13 +138,12 @@ class ExpenseTracker extends Component {
               <th>Amount</th>
             </tr>
           </thead>
-          <tbody>
-         
-          </tbody>
+          <tbody></tbody>
         </Table>
       </Container>
     );
   }
 }
+
 
 export default ExpenseTracker;
